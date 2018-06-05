0e4b72bac04f92f4fc55364e8c337f2c46f4396e
Giuseppe Sapienza <sapienza.giuseppe93@gmail.com>
2018-06-05 19:44:47 +0200

    Initial Commit

---
+++ WordpressKit/WordpressKit.h
@@ -0,0 +1,19 @@
+//
+//  WordpressKit.h
+//  WordpressKit
+//
+//  Created by Giuseppe Sapienza on 05/06/18.
+//  Copyright © 2018 Giuseppe Sapienza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WordpressKit.
+FOUNDATION_EXPORT double WordpressKitVersionNumber;
+
+//! Project version string for WordpressKit.
+FOUNDATION_EXPORT const unsigned char WordpressKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordpressKit/PublicHeader.h>
+
+
